4a528e45b308a76ea20cacd0bdeb813af7c5bd3a
unknown <muslumcelik47>
2024-10-13 22:00:34 +0300

    theCurrency-App
    theCurrency-App = it is my currency app that i ended it up in a few our

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import "./App.css";
+import Currency from "./components/Currency";
+
+function App() {
+  return (
+    <div 
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Currency />
+    </div>
+  );
+}
+
+export default App;
